14431ece600e474454eac3914d25790114150efd
Bea Sauve <Brittney.Sauve@SeattleColleges.edu>
2025-12-08 16:54:54 -0800

    Updated documentation style, and added some failsafes within methods.

--- user_profile.py
+++ user_profile.py
@@ -3,8 +3,9 @@ Building class to construct User Profiles
 '''
 
 class UserProfile:
-    ALLOWED_RELATIONS = {"single", "in a relationship", "married"}
+    
     def __init__(self, name, location, relationship_status, age, occupation, astrological_sign, status=""):
+        '''The constructor for the class'''
         self.name = name
         self.location = location
         self.relationship_status = relationship_status
@@ -15,86 +16,121 @@ class UserProfile:
         self.friends = []
         self.photo = "Photo not avalible"
 
-
     def get_name(self):
-        ''' Obtiain Users name
-        @return name(string): Users name'''
+        ''' Obtiain Users name.
+
+        :rtype string: name, Users name.
+        '''
         return self.name
     
     def set_name(self, name):
-        ''' Modify name of users profile'''
+        ''' Modify name of users profile.
+        
+        :type string: name, Name to add to profile.
+        '''
         self.name = name
 
-
     def get_location(self):
-        ''' Obtain Users location
-        @return location(string): Users location'''
-        return self.location
+        ''' Obtain Users location.
 
+        :rtype string: location, Users location.
+        '''
+        return self.location
 
     def get_relationship_status(self):
         ''' Obtain Users relationship status
-        @return relationship_status(string): Users relationship status'''
+
+        :rtype string: relationship_status, Users relationship status.
+        '''
         return self.relationship_status
 
-
     def get_age(self):
-        '''Obtain users age
-        @return age(int): Users age'''
+        '''Obtain users age.
+
+        :rtype int: age, Users age.
+        '''
         return self.age
 
-
     def get_occupation(self):
-        '''Obtain users occupation
-        @return occupation(string): Users occupation'''
-        return self.occupation
+        '''Obtain users occupation.
 
+        :rtype string: occupation, Users occupation.
+        '''
+        return self.occupation
 
     def get_astrological_sign(self):
         '''Obtain Users astrological sign
-        @return: astrological_sign(string): Users astro sign'''
-        return self.astrological_sign
 
+        :rtype string: astrological_sign, Users astro sign.
+        '''
+        return self.astrological_sign
 
     def get_status(self):
         '''Obtain Users relationship status
-        @return status(string): Users relationship status'''
+
+        :rtype string: status, Users relationship status.
+        '''
         return self.status
 
-
-    def set_status(self, status):
-        '''Set users relationship status, within allowed relationship statuses'''
-        if status and status.lower() in self.ALLOWED_RELATIONS:
-            self.status = status
-        print("Invalid Status. Please enter: Single, In a Relationship, or Married")
-
+    def set_relationship(self, status):
+        '''Set users relationship status, within allowed relationship statuses
+        
+        :type string: status, relationship the user is in'''
+        allowed_relations = {"single", "in a relationship", "married", "1", "2", "3"}
+        if status and status.lower() in allowed_relations:
+            if status == "1" or status == "single":
+                self.status = "Single"
+            if status == "2" or status == "in a relationship":
+                self.status = "In a Relationship"
+            elif status == "3" or "married":
+                self.status = "Married"
+        print("Invalid Status. Please choose: 1)Single, 2)In a Relationship, 3)Married")
+    
+    def set_status(self, status= bool):
+        '''Set users activity status
+        
+        :type boolean: True if user is online, False if offline
+        '''
+        if status == True:
+            self.status = "Online"
+        else:
+            self.status = "Offline"
 
     def get_friends(self):
         '''Obtain all friends in friends list
-        @return friends(list): list of users friends'''
+
+        :rtype list: friends, list of users friends.'''
         return self.friends
 
-
     def add_friend(self, friend_profile):
-        '''Add a friend to friends list'''
+        '''Add a friend to friends list.
+        
+        :type any: friend_profile, User profile of friend.
+        '''
         self.friends.append(friend_profile)
 
-
     def remove_friend(self, friend_profile):
-        '''Remove a friend from friends list'''
+        '''Remove a friend from friends list
+        
+        :type any: friend_profile, profile to remove from list
+        '''
         self.friends.remove(friend_profile)
 
-
     def print_details(self):
-        '''Display all details of user'''
+        '''Display all details of user.
+        
+        :rtype string: Details of users profile.
+        '''
         print(f'Image: {self.photo}/nStatus: {self.status}/nName: {self.name}/n'
               f'Age: {self.age:>5} Occupation: {self.occupation:>22} Location: {self.location}/n'
               f'Relationship Status: {self.relationship_status:>18} Astrological Sign: {self.astrological_sign} /n'
               f'Friends List: {self.friends}')
 
-
     def add_photo(self, photo):
-        '''Add a photo to profile'''
+        '''Add a photo to profile.
+        
+        :type string: photo, String reference to photo.
+        '''
         '''User may add a profile photo if they choose'''
         substring = ".jpg"
         if substring in photo:
